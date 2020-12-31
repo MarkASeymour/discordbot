1fe391957d55ef1f0cfd495beb5c4bd41dd72e95
Mark Seymour <seymour.mark9@gmail.com>
2020-12-31 16:18:25 -0500

    add beginning logic for nickname changes

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -9,20 +9,27 @@ public class PingPong extends ListenerAdapter {
 
     @Override
     public void onMessageReceived(MessageReceivedEvent e) {
-        if(e.getMessage().getContentRaw().equals("ping")) {
-            e.getChannel().sendMessage("pong").queue();
-            System.out.println(e.getAuthor());
-        }
-        if(e.getAuthor().getName().equals("Markinho")) {
-             for(Member member : e.getGuild().getMembers()) {
-                 String name = member.getUser().getName();
-                 System.out.println(name);
-                 if(name.equals("Markinho")) {
-                     System.out.println("c*&^&*%^&^%$^%$");
-                     member.modifyNickname("mark modified name");
-                 }
-             }
-        }
+
+//        if(e.getMessage().getContentRaw().equalsIgnoreCase("ping")) {
+//            e.getChannel().sendMessage("pong").queue();
+//            System.out.println(e.getAuthor());
+//        }
+            if(true) {
+                for (Member member : e.getGuild().getMembers()) {
+                    System.out.println("for loop hit");
+                    String name = member.getUser().getName();
+                    String nickName = member.getNickname();
+
+                    if (name.equals("bluedream_5gr")) {
+                        System.out.println(name);
+                        member.modifyNickname("cody - test").queue();
+                        System.out.println("Name modified");
+                    }
+
+
+                }
+            }
+
     }
 
 }
